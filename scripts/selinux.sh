baffb0f7540526c03e46a5fefb2e866df144e3a6
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-01-29 20:54:48 -0600

    changed a name

--- scripts/selinux.sh
+++ scripts/selinux.sh
@@ -6,7 +6,7 @@ sudo sed -i --follow-symlinks 's/selinux=0 //g' /boot/grub/grub.conf
 sudo sed -i --follow-symlinks 's/^kernel.*/& selinux=1 security=selinux/g' /boot/grub/grub.conf
 
 cd /tmp/modules
-make -f /usr/share/selinux/devel/Makefile quinovas.pp
-sudo semodule -i quinovas.pp
+make -f /usr/share/selinux/devel/Makefile quinovas-selinux.pp
+sudo semodule -i quinovas-selinux.pp
 
 sudo touch /.autorelabel
